c79e66ef2cb5fa56ccbccf3eca01cdfd43eb388b
Brandon Antonelli <bantonelli07@gmail.com>
2015-02-09 17:25:27 -0500

    Collect static assets for admin and browsable API

--- CustomDrumSamplesAPI/settings.py
+++ CustomDrumSamplesAPI/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = '/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'CustomDrumSamples/static'),
+    #os.path.join(BASE_DIR, 'CustomDrumSamples/static'),
 )
 
 # List of finder classes that know how to find static files in
